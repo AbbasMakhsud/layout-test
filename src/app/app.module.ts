20572e73bd449fe9a95cee38e77debf1b4927bac
Abbas Makhsudov <abbas.makhsudov@timesoft.local>
2019-12-13 15:47:15 +0400

    header/footer created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,21 @@ import { AppComponent } from './app.component';
 import {HomeComponent} from './pages/home/home.component';
 import {RoutingModule} from './routing/routing.module';
 import { ProductViewComponent } from './pages/product-view/product-view.component';
+import {RouterModule} from '@angular/router';
+import { HeaderComponent } from './shared-components/header/header.component';
+import { FooterComponent } from './shared-components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ProductViewComponent
+    ProductViewComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule,
     RoutingModule
   ],
   providers: [],
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -13,6 +13,21 @@ export class HomeComponent implements OnInit {
     { name: 'LOCAL STORE'},
     { name: 'LOOK BOOK'},
   ];
+
+  lookBooks = [
+    { name: "Men's",
+      description: 'Lorem ipsum dolor sit amet eras facilisis consectetur adipiscing elit lor, integer lorem consecteur dignissim laciniqui.',
+      actionName: 'Mens collection'
+    },
+    { name: "Women's",
+      description: 'Lorem ipsum dolor sit amet eras facilisis consectetur adipiscing elit lor, integer lorem consecteur dignissim laciniqui.',
+      actionName: 'Women collection'
+    },
+    { name: "Kid's",
+      description: 'Lorem ipsum dolor sit amet eras facilisis consectetur adipiscing elit lor, integer lorem consecteur dignissim laciniqui.',
+      actionName: 'Kid collection'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
