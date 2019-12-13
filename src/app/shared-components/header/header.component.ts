3f98158822ac35b5d4fe061f9e89115fdf6c82f8
unknown <azer.hashimov@knexel.com>
2019-12-13 16:22:12 +0400

    pages

--- src/app/shared-components/header/header.component.ts
+++ src/app/shared-components/header/header.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  menus = [
+    { name: 'MEN'},
+    { name: 'WOMEN'},
+    { name: 'THE BRAND'},
+    { name: 'LOCAL STORE'},
+    { name: 'LOOK BOOK'},
+  ];
   constructor() { }
 
   ngOnInit() {
